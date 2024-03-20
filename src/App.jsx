71b0f185cafe380740d86962f6f731ace9307319
spaceTony9 <bilokon.fw7@gmail.com>
2024-03-20 20:49:43 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React from 'react';
+import './App.css';
 
-import Square from "./components/square.jsx";
+import Square from './components/square.jsx';
 
 export default function App() {
-    return <>
-        <Square/>
-    </>
+  return <><Square /><Square /><Square /><Square /><Square /><Square /><Square /><Square /><Square /></>;
 }
